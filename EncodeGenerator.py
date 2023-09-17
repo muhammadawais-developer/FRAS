35d0bf6101bae2f1a287249ea5a9d8f27d1917dc
Muhammad Awais <awais.dev2serve@gmail.com>
2023-09-18 01:12:30 +0500

    face detection working

---
+++ EncodeGenerator.py
@@ -0,0 +1,42 @@
+#import dependencies
+import cv2
+import os
+import face_recognition
+import pickle
+
+# importing the student images into a list
+folderPath = 'Images'
+pathList = os.listdir(folderPath)
+imgList = []
+studentIds = []
+
+# getting the students pictures with ids
+for path in pathList:
+    imgList.append(cv2.imread(os.path.join(folderPath, path)))
+    studentIds.append(os.path.splitext(path)[0])
+print(studentIds)
+
+# opencv uses bgr
+# face_recognition uses rgb
+# so we are converting our images into rgb , its basically for encoding purposes
+def findEncodings(imagesList):
+    encodeList = []
+    for img in imagesList:
+        # we convert from bgr to rgb
+        img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img)[0]
+        encodeList.append(encode)
+    return encodeList
+
+print("Encoding Started...")
+encodeListKnown = findEncodings(imgList)
+encodeListKnownWithIds = [encodeListKnown, studentIds]
+print("Encoding Completed !")
+
+# pickle file creationg and storing the data for the webcam recognition
+
+file = open("EncodeFile.p","wb")
+pickle.dump(encodeListKnownWithIds,file)
+file.close()
+print("File Saved !")
+
--- main.py
+++ main.py
@@ -1,17 +1,66 @@
-# This is a sample Python script.
+# packagaes import
+import os
+import pickle
+import numpy as np
+import cv2
+import face_recognition
+import cvzone
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# webcam initialization
+cap = cv2.VideoCapture(0)
+cap.set(3, 640)
+cap.set(4, 480)
+imgBackground = cv2.imread("Resources/background.png")
 
+# importing the mode images into a list
+folderModePath = 'Resources/Modes'
+modePathList = os.listdir(folderModePath)
+imgModeList = []
+for path in modePathList:
+    imgModeList.append(cv2.imread(os.path.join(folderModePath,path)))
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+#load the encoding file for the webcam face recognition
 
+print("Loading Encode File ...")
+file = open('EncodeFile.p', 'rb')
+encodeListKnownWithIds = pickle.load(file)
+file.close()
+encodeListKnown, studentIds = encodeListKnownWithIds
+print("Encodings", studentIds)
+print("Encode File Loaded ...")
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+while True:
+    success, img = cap.read()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # resizing the img into 1/4th of the actual size of the img
+    imgS = cv2.resize(img,(0, 0), None, 0.25, 0.25)
+    imgS = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+    # locating the img and then compare the previous encoding with the new encodings
+    faceCurFrame = face_recognition.face_locations(imgS)
+    encodeCurFrame = face_recognition.face_encodings(imgS,faceCurFrame)
+
+    imgBackground[162:162+480, 55:55+640] = img
+    imgBackground[44:44+633, 808:808+414] = imgModeList[0]
+
+    for encodeFace, faceLoc in zip(encodeCurFrame,faceCurFrame):
+        matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
+        faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
+        # print('matches', matches)
+        # print('faceDis', faceDis)
+
+        matchIndex = np.argmin(faceDis)
+        # print('matchIndex', matchIndex)
+
+        if matches[matchIndex]:
+            print("Known Face Detected !")
+            print(studentIds[matchIndex])
+            y1,x2,y2,x1 = faceLoc
+            # y1,x2,y2,x1 = y1*4,x2*4,y2*4,x1*4
+            bbox =  55 + x1 , 162 + y1 , x2-x1, y2-y1
+            imgBackground = cvzone.cornerRect(imgBackground, bbox, rt=0)
+
+    # cv2.imshow("Webcam", img)
+    cv2.imshow("Face Attendance", imgBackground)
+    cv2.waitKey(1)
 
