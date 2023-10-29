478344a50486836a6cad7446d42d9a3df5a91316
muhammadawais-developer <awais.dev2serve@gmail.com>
2023-10-29 13:16:39 +0500

    fyp completed

--- AddDataToDatabase.py
+++ AddDataToDatabase.py
@@ -1,5 +1,89 @@
 import firebase_admin
 from firebase_admin import credentials
+from firebase_admin import db
+import random
 
-cred = credentials.Certificate("path/to/serviceAccountKey.json")
-firebase_admin.initialize_app(cred)
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred, {'databaseURL': "https://fras-eeb4b-default-rtdb.asia-southeast1.firebasedatabase.app/",
+                               'storageBucket': "fras-eeb4b.appspot.com"})
+
+ref = db.reference('Students')
+
+
+data = {
+    "1": {
+        "name": "Areeba",
+        "major": "CS",
+        "starting_year": random.randint(2015, 2023),
+        "total_attendance": random.randint(1, 50),
+        "standing": random.choice(["G", "B"]),
+        "year": random.randint(1, 5),
+        "last_attendance_time": "2022-12-11 00:54:34"
+        # "last_attendance_time": "2022-12-11 00:54:34"
+    },  "2": {
+        "name": "Awais",
+        "major": "SE",
+        "starting_year": random.randint(2015, 2023),
+        "total_attendance": random.randint(1, 50),
+        "standing": random.choice(["G", "B"]),
+        "year": random.randint(1, 5),
+        "last_attendance_time": "2022-12-11 00:54:34"
+    },
+    "5": {
+        "name": "Fawad",
+        "major": "Chemistry",
+        "starting_year": random.randint(2015, 2023),
+        "total_attendance": random.randint(1, 50),
+        "standing": random.choice(["G", "B"]),
+        "year": random.randint(1, 5),
+         "last_attendance_time": "2022-12-11 00:54:34"
+    },
+    "6": {
+        "name": "Saeed",
+        "major": "Mechanical",
+        "starting_year": random.randint(2015, 2023),
+        "total_attendance": random.randint(1, 50),
+        "standing": random.choice(["G", "B"]),
+        "year": random.randint(1, 5),
+        "last_attendance_time": "2022-12-11 00:54:34"
+    },
+    "7": {
+        "name": "Cruise",
+        "major": "Astronomy",
+        "starting_year": random.randint(2015, 2023),
+        "total_attendance": random.randint(1, 50),
+        "standing": random.choice(["G", "B"]),
+        "year": random.randint(1, 5),
+         "last_attendance_time": "2022-12-11 00:54:34"
+    },
+    "3": {
+        "name": "Elon",
+        "major": "Chemistry",
+        "starting_year": random.randint(2015, 2023),
+        "total_attendance": random.randint(1, 50),
+        "standing": random.choice(["G", "B"]),
+        "year": random.randint(1, 5),
+        "last_attendance_time": "2022-12-11 00:54:34"
+    },
+    "4": {
+        "name": "Emma",
+        "major": "Biology",
+        "starting_year": random.randint(2015, 2023),
+        "total_attendance": random.randint(1, 50),
+        "standing": random.choice(["G", "B"]),
+        "year": random.randint(1, 5),
+        "last_attendance_time": "2022-12-11 00:54:34"
+    },   "8": {
+        "name": "Sofiyan",
+        "major": "English",
+        "starting_year": random.randint(2015, 2023),
+        "total_attendance": random.randint(1, 50),
+        "standing": random.choice(["G", "B"]),
+        "year": random.randint(1, 5),
+        "last_attendance_time": "2022-12-11 00:54:34"
+    },
+}
+
+
+for key, value in data.items():
+    ref.child(key).set(value)
--- EncodeGenerator.py
+++ EncodeGenerator.py
@@ -1,42 +1,54 @@
-#import dependencies
 import cv2
-import os
 import face_recognition
 import pickle
+import os
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+from firebase_admin import  storage
 
-# importing the student images into a list
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred, {'databaseURL': "https://fras-eeb4b-default-rtdb.asia-southeast1.firebasedatabase.app/",
+                               'storageBucket': "fras-eeb4b.appspot.com"})
+
+
+# Importing student images
 folderPath = 'Images'
 pathList = os.listdir(folderPath)
+print(pathList)
 imgList = []
 studentIds = []
-
-# getting the students pictures with ids
 for path in pathList:
     imgList.append(cv2.imread(os.path.join(folderPath, path)))
     studentIds.append(os.path.splitext(path)[0])
+
+    fileName = f'{folderPath}/{path}'
+    bucket = storage.bucket()
+    blob = bucket.blob(fileName)
+    blob.upload_from_filename(fileName)
+
+
+    # print(path)
+    # print(os.path.splitext(path)[0])
 print(studentIds)
 
-# opencv uses bgr
-# face_recognition uses rgb
-# so we are converting our images into rgb , its basically for encoding purposes
+
 def findEncodings(imagesList):
-    # storing the encoded images
     encodeList = []
     for img in imagesList:
-        # we convert from bgr to rgb
-        img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         encode = face_recognition.face_encodings(img)[0]
         encodeList.append(encode)
+
     return encodeList
 
-print("Encoding Started...")
+
+print("Encoding Started ...")
 encodeListKnown = findEncodings(imgList)
 encodeListKnownWithIds = [encodeListKnown, studentIds]
-print("Encoding Completed !")
+print("Encoding Complete")
 
-# pickle file creation and storing the data for the webcam recognition
-file = open("EncodeFile.p","wb")
-pickle.dump(encodeListKnownWithIds,file)
+file = open("EncodeFile.p", 'wb')
+pickle.dump(encodeListKnownWithIds, file)
 file.close()
-print("File Saved !")
-
+print("File Saved")
